7c0c0ebee0aadfc414c4dcad07d5aadfdeb9ab5c
Rishabh Tiwari <rishabh.tiwari0504@gmail.com>
2025-02-12 01:21:42 +0400

    fix: build fixes

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,7 @@ async function bootstrap() {
   );
   app.enableCors(CORS_CONFIG);
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   app.use(cookieParser());
 
   const config = new DocumentBuilder()
