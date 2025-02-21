057b38fe658a81b8935e807ed5534ee00b98a217
Rishabh Tiwari <formymovies15@gmail.com>
2025-02-21 18:46:12 +0400

    feat : agent tool integration (#10)
    * feat: Agent tool integration
    
    * fix: lint fixed
    
    * fix: lint fixed
    
    * fix: lint fixed

--- src/modules/tools/tools.module.ts
+++ src/modules/tools/tools.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ToolsService } from './tools.service';
 import { ToolsController } from './tools.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
-
 @Module({
   providers: [ToolsService],
   controllers: [ToolsController],
