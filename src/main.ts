ad955e8e3b454523a01271dc3fea0d0017d51a63
Rishabh Tiwari <rishabh.tiwari0504@gmail.com>
2025-02-23 22:36:17 +0400

    fix: port fixing for prod/local

--- src/main.ts
+++ src/main.ts
@@ -24,6 +24,6 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(process.env.APP_SWAGGER_APP_PREFIX || '', app, document);
-  await app.listen(3000);
+  await app.listen(process.env.APP_PORT || '');
 }
 bootstrap();
